ca977c274ee87c4653246c2bd0d192a8bb0488ff
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-05-04 16:20:35 -0300

    Feat: Implementa logica das rotas

---
+++ routes/postRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = express.Router();
+
+// routes.post('/');
+// routes.get('/');
+// routes.get('/:id');
+// routes.put('/:id');
+// routes.delete('/me');
+
+module.exports = routes;
---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const routes = express.Router();
+
+// routes.post('/');
+// routes.get('/');
+// routes.get('/:id');
+// routes.delete('/me');
+
+module.exports = routes;
